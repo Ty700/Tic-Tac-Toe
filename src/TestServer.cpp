fc5eeb698b5a417bc4a3dcf5efe8efce85644693
Tyler Scotti <T.ScottiBusiness@yahoo.com>
2025-07-09 18:56:08 -0500

    Add unique Game ID to test server. And GET API to recieve game status of a given game id

--- src/TestServer.cpp
+++ src/TestServer.cpp
@@ -17,15 +17,18 @@ int createGameId()
 int main()
 {
 	httplib::Server svr;
-
+	
+	/* Home page */
 	svr.Get("/", [](const httplib::Request&, httplib::Response& res) {
 			res.set_content("Hello from TicTacToeServer!", "text/plain");
 		});
-
+	
+	/* Get health of the server */
 	svr.Get("/health", [](const httplib::Request&, httplib::Response& res) {
 			res.set_content("Server is running!", "text/plain");
 		});
-
+	
+	/* Test data for game */
 	svr.Get("/game", [](const httplib::Request&, httplib::Response& res) {
 	    std::string gameData = "{\n";
 	    gameData += "  \"gameId\": \"ABC123\",\n";
@@ -42,17 +45,31 @@ int main()
 	});
 
 	std::cout << "Starting server" << std::endl;
-		
+	
+	/* Create game using: curl -X POST  -d '{}' http://localhost:8080/create-game */
 	svr.Post("/create-game", [](const httplib::Request& req, httplib::Response& res)
 			{
 				std::string resp = "{\n";
 				resp += " \"message\": \"Game created!\", \n";
-				resp += " \"gameId\": \"GAME" + std::to_string(createGameId()) + "\"\n";
+				resp += " \"gameId\": \"game/" + std::to_string(createGameId()) + "\"\n";
 				resp += "}\n";
 
 				res.set_content(resp, "application/json");
 			});
+	
+	/* Once game has been created, we can view the status */
+	svr.Get(R"(/game/(.+))", [](const httplib::Request& req, httplib::Response& res) 
+			{
+				std::string gameId = req.matches[1];
 
+				std::string gameData = "{\n";
+				gameData += " \"gameID\": \"" + gameId + "\", \n";
+				gameData += " \"status\": \"in-progress\", \n";
+				gameData += " \"currentPlayer\": \"Player1\"\n";
+				gameData += "}";
+
+				res.set_content(gameData, "application/json");
+			});
 
 	svr.listen("localhost", 8080);
 	
