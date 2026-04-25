5eb1f3c3270489bd1d24685fca7c6169a7009546
Tyler <tyler@Tylers-MacBook-Pro.local>
2026-04-25 17:55:57 -0500

    Init for iOS

---
+++ iOS/Tic-Tac-Toe/Tic-Tac-Toe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tic-Tac-Toe
+//
+//  Created by Tyler on 4/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
